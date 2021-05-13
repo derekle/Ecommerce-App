9d88d17a49021347d8434441ea0b16b9dec3f80b
derek <derekle.creative@gmail.com>
2021-05-12 18:44:28 -0700

    setup for session controller; DRY cleanup

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -3,12 +3,14 @@ class SessionController < ApplicationController
     
     ## signup logic
     get '/signup' do
+        erb :'session/signup'
     end
     post '/sigup' do
     end
 
     ## login logic
     get '/login' do
+        erb :'session/login'
     end
     post '/login' do
     end
