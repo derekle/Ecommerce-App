36633b6e2f8572da257f7f3bbb58f216c31a4a5d
derek <derekle.creative@gmail.com>
2021-05-12 17:46:22 -0700

    add helpers section, enable sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,17 @@
 class ApplicationController < Sinatra::Base
   ## control app startup and general logic ##
-    configure do
-        set :views, 'app/views'
-    end
+  # configuration
+  configure do
+      set :views, 'app/views'
+      # A session is used to keep state during requests. If activated, you have one session hash per user session: #
+      enable :sessions
+  end
 
-    get '/' do
-      erb :index
-    end
-  end 
+  get '/' do
+    erb :index
+  end
+
+  # define helper methods for use in route handlers and templates: #
+  helpers do
+  end
+end 
