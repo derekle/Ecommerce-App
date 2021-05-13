bdf823daa6d0b76ae91632a17e3077df05d57022
derek <derekle.creative@gmail.com>
2021-05-12 18:10:12 -0700

    setup session secret

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,9 @@ class ApplicationController < Sinatra::Base
       set :views, 'app/views'
       # A session is used to keep state during requests. If activated, you have one session hash per user session: #
       enable :sessions
+      # Throws an exception if SESSION_SECRET does not exist in the current ENV. Set random hex for session secret. 
+      # Solution found from sinatra documentation and user grempe in this thread: https://github.com/sinatra/sinatra/issues/1187
+      set :session_secret, ENV.fetch('SESSION_SECRET') { SecureRandom.hex(64) }      
   end
 
   get '/' do
--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,7 @@
 ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
-
+# Replace the userspace Ruby (OpenSSL) RNG with `/dev/urandom`
+require 'sysrandom/securerandom'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
 ActiveRecord::Base.establish_connection(
