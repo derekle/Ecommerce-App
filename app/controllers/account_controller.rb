b9fb482d0a88c3be4f43c9f85e99408f350ef227
derek <derekle.creative@gmail.com>
2021-05-19 08:14:19 -0500

    remove unused routes

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -1,15 +1,15 @@
 class AccountController < ApplicationController
 	## control account CRUD logic. Users MUST be logged in to view account controls. ##
-		#index 	index page to display account actions, show account details for transactions
+		#index - index page to display account actions, show account details for transactions
 	get '/account' do
 		# redirect_if_not_logged_in
 		erb :'user/account'               
 	end
-	#show   	displays one account based on ID in the url
+	    #show - displays one account based on ID in the url
 	get '/account/:id' 	do
 		erb :'user/show'    
 	end
-		#edit 	displays edit form based on ID in the url
+		#edit - displays edit form based on ID in the url
 	get '/account/:id/edit/name' do
 		redirect_if_not_logged_in	  
 		erb :'user/edit/name'    
@@ -22,7 +22,7 @@ class AccountController < ApplicationController
 		redirect_if_not_logged_in	  
 		erb :'user/edit/delete'    
 	end
-		#update 	modifies an existing account based on ID in the url
+		#update - modifies an existing account based on ID in the url
 	patch '/account/:id'	do
 		redirect_if_not_logged_in 
 		if !params[:username].empty?
@@ -32,11 +32,7 @@ class AccountController < ApplicationController
 			redirect to "/account"
 		end     
 	end
-		#update 	replaces an existing account based on ID in the url
-	put '/account/:id'	  do 
-		redirect_if_not_logged_in         
-	end
-		#delete 	deletes one account based on ID in the url
+		#delete - deletes one account based on ID in the url
 	delete  '/account/:id'	  do
 		redirect_if_not_logged_in 
 		current_user.delete
