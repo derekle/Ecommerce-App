8af0cf71df003c67c3b25f6477f3e47ee4d5746e
derek <derekle.creative@gmail.com>
2021-05-19 08:36:52 -0500

    setup routes

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -1,3 +1,28 @@
 class ProductController < ApplicationController
     ## control product CRUD logic. ##
+
+    # index product - index page to display all products
+    get '/products' do
+    end
+    # new product - displays create product form
+    get '/products/create' do
+    end
+    # create product - creates one product
+    post '/products' do
+    end
+    # show product - displays one product based on ID in the url
+    get '/products/:id' do
+    end
+    # edit product - displays edit form based on ID in the url
+    get '/products/:id/edit' do
+    end
+    # update product - modifies an existing product based on ID in the url
+    patch '/products/:id' do
+    end
+    # update product - replaces an existing product based on ID in the url
+    put '/products/:id' do
+    end
+    # delete product - deletes one product based on ID in the url
+    delete '/products/:id' do
+    end
 end
